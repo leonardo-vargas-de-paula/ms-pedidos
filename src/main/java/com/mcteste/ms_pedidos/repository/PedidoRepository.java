a467d7b3853797245c6768ca62243ce91cf70cbc
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-10 12:33:11 -0400

    feat: add PedidoRepository

---
+++ src/main/java/com/mcteste/ms_pedidos/repository/PedidoRepository.java
@@ -0,0 +1,7 @@
+package com.mcteste.ms_pedidos.repository;
+
+import com.mcteste.ms_pedidos.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+}
