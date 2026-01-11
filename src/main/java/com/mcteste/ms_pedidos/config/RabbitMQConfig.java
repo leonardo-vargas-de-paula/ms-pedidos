3cfeee1c23a956646275ddb7228d385717dac868
leonardo-vargas-de-paula <leonardovargasdpaula@gmail.com>
2026-01-10 20:06:28 -0400

    feat: add rabbitmqconfig

---
+++ src/main/java/com/mcteste/ms_pedidos/config/RabbitMQConfig.java
@@ -0,0 +1,25 @@
+package com.mcteste.ms_pedidos.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import tools.jackson.databind.json.JsonMapper;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${broker.queue.processamento.name}")
+    private String queue;
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter(JsonMapper jsonMapper){
+
+        return new JacksonJsonMessageConverter(jsonMapper);
+    }
+
+
+
+
+}
